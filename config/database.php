412460305f8f86275d1c70249f7733939e1c5cc8
bios123456789 <51689277+bios123456789@users.noreply.github.com>
2019-08-27 11:27:38 +0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -42,9 +42,9 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('remotemysql.com', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
+            'database' => env('h7owARCixu', 'forge'),
             'username' => env('h7owARCixu', 'forge'),
             'password' => env('ZfNQ4Fb4P4', ''),
             'unix_socket' => env('DB_SOCKET', ''),
