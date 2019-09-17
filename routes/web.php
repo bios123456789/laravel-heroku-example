0bd869fe4b37dbed7335e5f41c087f9293779fe9
bios123456789 <51689277+bios123456789@users.noreply.github.com>
2019-09-17 10:50:36 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('users', function() {
-    return '<h1>users! 555</h1>';
+Route::get('showname', function() {
+    return '<h1>Hello laravel</h1>';
 });    
 Auth::routes();
 
