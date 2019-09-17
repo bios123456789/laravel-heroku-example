e46ef5a018c16db3f562dfe7409652ee187659e9
bios123456789 <51689277+bios123456789@users.noreply.github.com>
2019-09-17 11:43:57 +0700

    Update HelloControllers.php

--- app/Http/Controllers/HelloControllers.php
+++ app/Http/Controllers/HelloControllers.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class HelloController extends Controller
 {
-    function showHello(){
-       return '<h1>Hello Controller : Bios witchapol</h1>';
+    function showHello($name){
+       return '<h1>Hello:'.$name.'</h1>';
     }
 }
