0ddfb6734846ece923cc11bc449f0dbea7c6c7c3
bios123456789 <51689277+bios123456789@users.noreply.github.com>
2019-09-17 11:51:40 +0700

    Update HelloControllers.php

--- app/Http/Controllers/HelloControllers.php
+++ app/Http/Controllers/HelloControllers.php
@@ -9,4 +9,7 @@ class HelloController extends Controller
     function showHello($name){
        return '<h1>Hello:'.$name.'</h1>';
     }
-}
+    function resultProduct($product){
+       return '<h1>show:'.$product.'</h1>';
+    }
+}    
